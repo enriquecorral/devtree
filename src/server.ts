b46058c3f4381779ea76f1e1f1d4d4f2ba0c5bf8
Enrique Corral <enriquecorral97@gmail.com>
2025-07-05 17:03:41 -0600

    Archivo de rutas

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+import router from './router';
 const app = express();
 
-// Rounting
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use('/api', router)
+
 
 export default app;
