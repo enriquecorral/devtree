57294ae96a308605fde47cfe627966db379ad580
Enrique Corral <enriquecorral97@gmail.com>
2025-07-04 23:57:09 -0600

    Usar sintaxis "module"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
 });
