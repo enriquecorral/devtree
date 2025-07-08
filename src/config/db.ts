10e8a71fb16ab95414e1baf781f21558dce5e0f0
Enrique Corral <enriquecorral97@gmail.com>
2025-07-07 22:47:11 -0600

    Añadir colors

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
+import colors from "colors";
 
 export const connectDB = async () => {
   try {
     const { connection } = await mongoose.connect(process.env.MONGO_URI)
     const url = `${connection.host}:${connection.port}`;
 
-    console.log(`MongoDB conectado en ${url}`);
+    console.log(colors.cyan.bold(`MongoDB conectado en ${url}`));
   }
   catch (error) {
-    console.error("Error al conectar a la base de datos:", error);
+    console.error(colors.bgRed.white.bold(`Error al conectar a la base de datos: ${error.message}`));
     process.exit(1); // Termina el proceso con un código de error
   }
 };
