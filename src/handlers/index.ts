ed0b1d3dae1326929570fd987d0d4519adaa2a15
Enrique Corral <enriquecorral97@gmail.com>
2025-08-21 20:20:02 -0600

    Hashear passwords

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
 
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   const userExists = await User.findOne({ email });
 
@@ -14,8 +15,7 @@ export const createAccount = async (req: Request, res: Response) => {
   }
 
   const user = new User(req.body)
-
+  user.password = await hashPassword(password)
   await user.save()
-
   res.status(201).send("Registro creado correctamente");
 }
