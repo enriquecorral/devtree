43c85a05f5ed80313b4115cf6b7de95ea2461bfd
Enrique Corral <enriquecorral97@gmail.com>
2025-08-25 22:40:51 -0600

    Validación

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,26 @@
 import { Router } from "express";
+import { body } from "express-validator"
 import { createAccount } from "./handlers";
 
 const router = Router();
 
 /* Autenticación y Registro */
-router.post("/auth/register", createAccount);
+router.post("/auth/register", 
+  body("handle")
+    .notEmpty()
+    .withMessage("El nombre de usuario no puede ir vacío"),
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre no puede ir vacío"),
+  body("email")
+    .isEmail()
+    .withMessage("El email no es válido"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña es muy corta, mínimo 8 caracteres"),
+  (req, res, next) => {
+    Promise.resolve(createAccount(req, res)).catch(next);
+  });
 
 
 export default router;
